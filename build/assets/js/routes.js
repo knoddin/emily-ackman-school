c132104df3ecbf03d9c7d4203629f62676731ab5
knoddin <knoddin@gmail.com>
2017-08-10 19:00:50 -0400

    adding build files

---
+++ build/assets/js/routes.js
@@ -0,0 +1,1 @@
+var foundationRoutes = [{"name":"home","url":"/home","path":"templates/home.html"},{"name":"bio","url":"/bio","path":"templates/bio.html"},{"name":"logo","url":"/","path":"templates/logo.html"}]; 
